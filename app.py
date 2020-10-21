b6255103afbaddac697e3b647d6a035cdc6d1728
Sergiocleber <sergio.cleberjr@gmail.com>
2020-10-20 21:26:57 -0300

    AC3

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+@app.route('/')
+def sequencia():
+   proximo = 1
+   anterior = 0
+   cont = 0
+   resposta = "0,"
+   while (found < limite):
+       var1 = proximo
+       proximo = proximo + anterior
+       anterior = var1
+       cont=cont+1
+       resposta+= str(proximo) + ","
+
+
+   return resposta
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
